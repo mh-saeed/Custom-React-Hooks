9540d40f67a83bab2baf3b36068c8d40f350707e
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-06 00:46:42 +0500

    added: useAsync hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@
 // import UpdateEffectComponent from "./04-useUpdateEffect/UpdateEffectComponent";
 // import ArrayComponent from "./05-useArray/ArrayComponent";
 // import PreviousComponent from "./06-usePrevious/PreviousComponent";
-import StateWithHistoryComponent from "./07-useStateWithHistory/StateWithHistoryComponent";
+// import StateWithHistoryComponent from "./07-useStateWithHistory/StateWithHistoryComponent";
+// import StorageComponent from "./08-useStorage/StorageComponent";
+import AsyncComponent from "./09-useAsync/AsyncComponent";
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
     // <UpdateEffectComponent />
     // <ArrayComponent />
     // <PreviousComponent />
-    <StateWithHistoryComponent />
+    // <StateWithHistoryComponent />
+    // <StorageComponent />
+    <AsyncComponent />
   );
 }
 
