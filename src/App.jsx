74e8c5ef74450bfce595208f4b49bb153733edcf
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-05 18:13:26 +0500

    added: useDebounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import CustomHookApp from "./00-customHook/CustomHookApp";
 // import ToggleComponent from "./01-useToggle/ToggleComponent";
-import TimeoutComponent from "./02-useTimeout/TimeoutComponent";
+// import TimeoutComponent from "./02-useTimeout/TimeoutComponent";
+import DebounceComponent from "./03-useDebounce/DebounceComponent";
 
 function App() {
   return (
     <>
       {/* <CustomHookApp /> */}
       {/* <ToggleComponent /> */}
-      <TimeoutComponent />
+      {/* <TimeoutComponent /> */}
+      <DebounceComponent />
     </>
   );
 }
