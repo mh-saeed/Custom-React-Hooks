b246323cb8a6c85791354eb87c1be11065429e7a
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-05 21:41:49 +0500

    added: useUpdateEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import CustomHookApp from "./00-customHook/CustomHookApp";
 // import ToggleComponent from "./01-useToggle/ToggleComponent";
 // import TimeoutComponent from "./02-useTimeout/TimeoutComponent";
-import DebounceComponent from "./03-useDebounce/DebounceComponent";
+// import DebounceComponent from "./03-useDebounce/DebounceComponent";
+import UpdateEffectComponent from "./04-useUpdateEffect/UpdateEffectComponent";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <CustomHookApp /> */}
       {/* <ToggleComponent /> */}
       {/* <TimeoutComponent /> */}
-      <DebounceComponent />
+      {/* <DebounceComponent /> */}
+      <UpdateEffectComponent />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,21 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  //  </React.StrictMode>
 );
+
+/* 
+Strict mode can’t automatically detect side effects for you, but it can help you spot them by making them a little more deterministic. This is done by intentionally double-invoking the following functions:
+
+1. Class component constructor, render, and shouldComponentUpdate methods
+2. Class component static getDerivedStateFromProps method
+3. Function component bodies
+4. State updater functions (the first argument to setState)
+5. Functions passed to useState, useMemo, or useReducer 
+
+If you remove the StrictMode element from index.js, you'll see the output only gets logged once
+
+Note: Strict mode checks are run in development mode only; they do not impact the production build.
+*/
