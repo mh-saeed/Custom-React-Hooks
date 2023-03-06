34edfc21cf1dcbfa4acd365df0c4757a36a05536
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-06 22:21:47 +0500

    added: useGeolocation hook

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,54 @@
 // import EventListenerComponent from "./13-useEventListener/EventListenerComponent";
 // import OnScreenComponentComponent from "./14-useOnScreen/OnScreenComponent";
 // import WindowSizeComponent from "./15-useWindowSize/WindowSizeComponent";
-import MediaQueryComponent from "./16-useMediaQuery/MediaQueryComponent";
+// import MediaQueryComponent from "./16-useMediaQuery/MediaQueryComponent";
+// import GeolocationComponent from "./17-useGeolocation/GeolocationComponent";
+// import StateWithValidationComponent from "./18-useStateWithValidation/StateWithValidationComponent";
+import SizeComponent from "./19-useSize/SizeComponent";
+// import EffectOnceComponent from "./20-useEffectOnce/EffectOnceComponent";
+// import ClickOutsideComponent from "./21-useClickOutside/ClickOutsideComponent";
+// import DarkModeComponent from "./22-useDarkMode/DarkModeComponent";
+// import CopyToClipboardComponent from "./23-useCopyToClipboard/CopyToClipboardComponent";
+// import CookieComponent from "./24-useCookie/CookieComponent";
+// import TranslationComponent from "./25-useTranslation/TranslationComponent";
+// import OnlineStatusComponent from "./26-useOnlineStatus/OnlineStatusComponent";
+// import RenderCountComponent from "./27-useRenderCount/RenderCountComponent";
+// import DebugInformationComponent from "./28-useDebugInformation/DebugInformationComponent";
+// import HoverComponent from "./29-useHover/HoverComponent";
+// import LongPressComponent from "./30-useLongPress.js/LongPressComponent";
 
 function App() {
-  return (
-    // <CustomHookApp />
-    // <ToggleComponent />
-    // <TimeoutComponent />
-    // <DebounceComponent />
-    // <UpdateEffectComponent />
-    // <ArrayComponent />
-    // <PreviousComponent />
-    // <StateWithHistoryComponent />
-    // <StorageComponent />
-    // <AsyncComponent />
-    // <FetchComponent />
-    // <ScriptComponent />
-    // <DeepCompareEffectComponent />
-    // <EventListenerComponent />
-    // <OnScreenComponentComponent />
-    // <WindowSizeComponent />
-    <MediaQueryComponent />
-  );
+  // return <ToggleComponent />
+  // return <TimeoutComponent />
+  // return <DebounceComponent />
+  // return <UpdateEffectComponent />
+  // return <ArrayComponent />
+  // return <PreviousComponent />
+  // return <StateWithHistoryComponent />
+  // return <StorageComponent />
+  // return <AsyncComponent />
+  // return <FetchComponent />
+  // return <ScriptComponent />
+  // return <DeepCompareEffectComponent />
+  // return <EventListenerComponent />
+  // return <OnScreenComponentComponent />
+  // return <WindowSizeComponent />
+  // return <MediaQueryComponent />
+
+  // return <GeolocationComponent />;
+  // return <StateWithValidationComponent />
+  return <SizeComponent />
+  // return <EffectOnceComponent />
+  // return <ClickOutsideComponent />
+  // return <DarkModeComponent />
+  // return <CopyToClipboardComponent />
+  // return <CookieComponent />
+  // return <TranslationComponent />
+  // return <OnlineStatusComponent />
+  // return <RenderCountComponent />
+  // return <DebugInformationComponent />
+  // return <HoverComponent />
+  // return <LongPressComponent />
 }
 
 export default App;
